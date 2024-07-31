69f5812d7f85e7162bd2aae0fc27239566df2c79
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-31 20:49:26 +0530

    Add files via upload

---
+++ join method.py
@@ -0,0 +1,7 @@
+str1 = "hello my name is sajin"
+emplist = str1.split(" ")
+print(emplist)
+# OUTPUT - ['hello', 'my', 'name', 'is', 'sajin']
+str = "-".join(emplist)
+print(str)
+#OUTPUT - hello-my-name-is-sajin
